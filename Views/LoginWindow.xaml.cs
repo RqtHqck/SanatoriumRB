f3b2ffb94cd4f85f17abe56781c7a4c527ae9cdf
Ihar <rathack13@gmail.com>
2024-12-05 10:59:10 +0300

    login and register window forms created

---
+++ Views/LoginWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Sanatorium.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Обработчик клика для кнопки "Login"
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string email = EmailTextBox.Text;
+            string password = PasswordBox.Password;
+            Console.WriteLine(email, password);
+
+            // Логика аутентификации пользователя
+            MessageBox.Show($"Пользователь: {email}, Пароль: {password}");
+        }
+
+        // Обработчик клика для кнопки "Register"
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide(); // Скрытие текущего окна (LoginWindow)
+            var registerWindow = new RegisterWindow(); // Открытие нового окна (RegisterWindow)
+            registerWindow.Show();
+        }
+
+    }
+}
---
+++ Views/RegisterWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Sanatorium.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для RegisterWindow.xaml
+    /// </summary>
+    public partial class RegisterWindow : Window
+    {
+        public RegisterWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Обработчик клика для кнопки "Login"
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide(); // Скрытие текущего окна (RegisterWindow)
+            var loginWindow = new LoginWindow(); // Открытие нового окна (LoginWindow)
+            loginWindow.Show();
+
+        }
+
+        // Обработчик клика для кнопки "Register"
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Логика для регистрации
+            string username = UsernameTextBox.Text;
+            string email = EmailTextBox.Text;
+            string password = PasswordBox.Password;
+
+            // Логика аутентификации пользователя
+            MessageBox.Show($"Пользователь: Имя: {username}, Почта: {email}, Пароль: {password}");
+        }
+    }
+}
